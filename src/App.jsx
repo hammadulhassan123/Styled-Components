088fa41108539322a345f90f437c081450eb476e
Hammad Ul Hassan <hammadulhassanqureshi@gmail.com>
2023-05-07 16:11:48 +0500

    git commit -m "finalcommit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Header from "./Components/header"
-import Footer from "./Components/Footer"
 import { Container } from "./Components/styled/Container.styled"
 import { ThemeProvider } from "styled-components"
 import content from "./content"
